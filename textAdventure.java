6ce847ff4d8ab636f2cfba7c17879302486e2a9e
Kharlo109 <kharlopena@gmail.com>
2021-09-12 17:28:00 -0400

    Created welcome function, battle menu, nad user input.

--- textAdventure.java
+++ textAdventure.java
@@ -4,10 +4,41 @@
 
 // A simple program designed as a text based adventure game
 
-public class textAdventure {
-    public static void main(String args[]) {
+import java.util.Scanner;
 
-        System.out.println("Testing");
-	System.out.println("1,2,3");
+public class TextAdventure {
+    public static void main(String[] args) {
+
+     welcome();
+     battle();
+     
+    }
+    public static void welcome() {
+
+      System.out.print("Please enter your name: ");
+      
+      String userInput = getUserInput();
+      
+      System.out.println(String.format("\nWelcome to the game %s!", userInput));
+  
+    }
+
+    public static void battle() {
+
+      System.out.println("\nA skeleton appears! What will you do?\n");
+      
+      System.out.println("1.) Attack\n2.) Run\n");
+    }
+
+    public static String getUserInput() {
+
+      Scanner input = new Scanner(System.in);
+
+      String userInput = input.nextLine();
+
+      input.close();
+
+      return userInput;
+
     }
 }
